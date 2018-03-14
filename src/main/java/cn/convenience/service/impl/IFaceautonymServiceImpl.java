4d518e612416cdf16f25777a7baafa7b75bcebc4
Your Name <you@example.com>
2018-03-14 16:48:12 +0800

    刷脸

--- src/main/java/cn/convenience/service/impl/IFaceautonymServiceImpl.java
+++ src/main/java/cn/convenience/service/impl/IFaceautonymServiceImpl.java
@@ -2,7 +2,11 @@ package cn.convenience.service.impl;
 
 import net.sf.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.httpclient.HttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +21,8 @@ import cn.convenience.utils.AES;
 import cn.convenience.utils.HttpRequest;
 import cn.convenience.utils.Util;
 import cn.sdk.bean.BaseBean;
+import cn.sdk.util.HttpClientUtil;
+import cn.sdk.util.HttpUtils;
 import cn.sdk.util.MsgCode;
 import cn.sdk.util.StringUtil;
 
@@ -46,8 +52,10 @@ private final Logger logger = LoggerFactory.getLogger(getClass());
 		BaseBean baen=new BaseBean();
 		String plainText = "a="+appid+"&m=getdetectinfo&t="+Util.getCurrentTime()+"&e="+600;
 		logger.info("请求参数:"+plainText);
-		String url=convenienceCache.getInfoUrl();
-		String secretkey=convenienceCache.getSecretkey();
+//		String url=convenienceCache.getInfoUrl();
+		String url = "https://iauth.wecity.qq.com/new/cgi-bin/getdetectinfo.php";
+//		String secretkey=convenienceCache.getSecretkey();
+		String secretkey="9828577231bdc6d01754e292023cdbb8";
 		String sig = Util.getSig(new String[]{token,appid});
 		String signature = Util.getSignature(secretkey, plainText);
 		String ret = HttpRequest.sendPost(url, "token="+token+"&appid="+appid+"&sig="+sig,signature,"application/x-www-form-urlencoded");
@@ -59,7 +67,8 @@ private final Logger logger = LoggerFactory.getLogger(getClass());
 		JSONObject respStr=JSONObject.fromObject(ret);
 		if("0".equals(respStr.get("errorcode").toString())){
 			 String data=respStr.get("data").toString();
-			 byte[] by=AES.decrypt(Base64.decodeBase64(data),convenienceCache.getAeskey().getBytes("UTF-8"));
+//			 byte[] by=AES.decrypt(Base64.decodeBase64(data),convenienceCache.getAeskey().getBytes("UTF-8"));
+			 byte[] by=AES.decrypt(Base64.decodeBase64(data),"26cb3f325891d42bec10efdeec9a4f95".getBytes("UTF-8"));
 			 String str=new String(by, "UTF-8");
 			 JSONObject json=JSONObject.fromObject(str);
 			 logger.info("获取用户信息:"+json.getString("ID")+":"+json.getString("name"));
